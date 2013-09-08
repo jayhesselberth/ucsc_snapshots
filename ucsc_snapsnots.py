b531ad0388ad7d562679b2715963ff69a25e7ed1
Jay Hesselberth <jay.hesselberth@gmail.com>
2013-09-08 12:23:46 -0600

    updated to SVN revision 543

--- ucsc_snapsnots.py
+++ ucsc_snapsnots.py
@@ -32,56 +32,110 @@ from path import path
 
 __author__ = 'Jay Hesselberth'
 __contact__ = 'jay.hesselberth@gmail.com'
-__version__ = '$Revision: 538 $'
+__version__ = '$Revision: 543 $'
 
-def ucsc_snapshots(bedfilename, session_id, verbose):
+def ucsc_snapshots(bedfilename, session_id, rev_display, img_types,
+                   no_delay, verbose):
 
     ucscsession.settings.hgsid = session_id
-    session = Session()
+    session = Session(rev_display, no_delay, verbose)
 
     # for each BED field, set params on the UCSC browser and fetch the
     # images
-    for region in BedTool(open(bedfilename)):
+    for region in BedTool(bedfilename):
     
-        position = '%s:%s-%s' % (region.chrom, region.start,
-                                 region.end)
+        position = '%s:%s-%s' % (region.chrom, region.start, region.end)
 
         session.set_position(position)
 
-        pdffile = getfilename(session_id, position,
-                              name=region.name, score=region.score)
+        for imgtype in img_types:
 
-        session.pdf(filename=pdffile)
-
-        if verbose:
-            print >>sys.stderr, ">> writing %s" % pdffile 
-
-        pngfile = getfilename(session_id, position, 
-                              name=region.name, score=region.score,
-                              ext='png')
-        session.png(filename=pngfile)
-
-        if verbose:
-            print >>sys.stderr, ">> writing %s" % pngfile 
+            filename = getfilename(session_id, position,
+                                   name=region.name, score=region.score,
+                                   ext=imgtype)
+            session.write_image(imgtype=imgtype, filename=filename,
+                                strand=region.strand)
 
 class Session(ucscsession.session._UCSCSession):
-    def png(self, position=None, filename=None):
+
+    def __init__(self, rev_display, no_delay, verbose):
+
+        ucscsession.session._UCSCSession.__init__(self)
+
+        self.verbose = verbose
+
+        # keep track of the flipped state in the session. if flipped is
+        # True, will show neg strand genes 5'->3'
+        self.rev_display = rev_display
+        flipped = self._find_flipped_state()
+
+        self.flipped = flipped
+
+        # XXX no_delay for debugging only
+        if no_delay:
+            self._SLEEP = 1
+        else:
+            # required minimum time between requests per UCSC
+            self._SLEEP = 15
+
+    def write_image(self, imgtype, filename, strand):
+        ''' wrapper for image generation. writes requested content to
+        filename.
+        
+        only PDF and PNG are currently supported.'''
+        # XXX could add more image capability by converting retrieved png
+        # files to jpeg, tif etc.
+        if imgtype.lower() == 'pdf':
+            content = self.pdf(strand)
+        elif imgtype.lower() == 'png':
+            content = self.png(strand)
+        else:
+            raise OSError, ">> image type %s not supported" % imgtype
+
+        with open(filename, 'w') as fileout:
+            fileout.write(content)
+
+        if self.verbose:
+            print >>sys.stderr, ">> wrote %s" % filename
+
+    def pdf(self, strand):
         """ 
-        Save a PNG of the current browser view.
+        retrieve PDF of the current browser view for currently set
+        position.
 
-        If `position` is None, then use the last available position.
+        returns: pdf content
+        """
+        payload = {'hgt.psOutput':'on'}
 
-        Returns the created filename.
+        # deal with display flipping
+        payload = self._flip_display(strand, payload)
+
+        response = self.session.post(self.tracks_url, data=payload)
+
+        time.sleep(self._SLEEP)
+
+        link = ucscsession.helpers.pdf_link(response)
+
+        content = self.session.get(link).content
+
+        return content
+
+    def png(self, strand):
+        """ 
+        retrieve merged PNG of the current browser view for currently set
+        position.
+
+        returns: png content
         """
         # these CGI settings ensure the browser returns a single, merged
         # PNG for the viewport
         payload = {'hgt.imageV1':1,
                    'hgt.trackImgOnly':1}
 
-        payload.update(position=self._position_string(position))
+        # deal with display flipping
+        payload = self._flip_display(strand, payload)
 
-        # example: 
-        # <IMG SRC='../trash/hgt/hgt_genome_6243_68cdb0.png'
+        # example: <IMG SRC='../trash/hgt/hgt_genome_6243_68cdb0.png'
         img_regex = re.compile('<IMG SRC=\'\.\.\/(trash\/hgt\/hgt_.*\.png)')
 
         response = self.session.post(self.tracks_url, data=payload)
@@ -89,20 +143,57 @@ class Session(ucscsession.session._UCSCSession):
 
         time.sleep(self._SLEEP)
 
-        base_url = 'http://genome.ucsc.edu/'
-        png_content = self.session.get(base_url + imgfilename)
+        link = 'http://genome.ucsc.edu/' + imgfilename
+        content = self.session.get(link).content
 
-        fout = open(filename, 'w')
-        fout.write(png_content.content)
-        fout.close()
+        return content
 
-        return filename
+    def _flip_display(self, strand, payload):
+        ''' flip display for such that all genes (pos and neg strands) are
+        shown 5'->3'
+        
+        adds hgt.toggleRevCmplDisp to CGI payload if required'''
+
+        # the CGI var is a true toggle so must be flipped back to e.g.
+        # pos strand genes after a neg strand gene is retrieved and vice
+        # versa
+
+        if strand == '-' and self.rev_display and not self.flipped:
+            payload['hgt.toggleRevCmplDisp'] = '1'
+            self.flipped = True
+
+        elif strand == '+' and self.rev_display and self.flipped:
+            payload['hgt.toggleRevCmplDisp'] = '1'
+            self.flipped = False
+
+        return payload
+
+    def _find_flipped_state(self):
+        ''' determine whether the display is currently flipped.
+        
+        returns: bool'''
+        cart_info = self.cart_info()
+        db = cart_info['db']
+        comp_key = 'hgt.revCmplDisp_%s' % db
+
+        try:
+            state = cart_info[comp_key]
+        except KeyError:
+            msg = ">> flipped state not avail in %s" % comp_key
+            raise KeyError, msg
+
+        # possible vals are 0 and 1
+        if int(state) == 0:
+            return False
+        else:
+            return True
 
 def getfilename(session_id, position, name, score, ext='pdf'):
-    ''' generate a filename for the PDF output. create directory if
-    needed.
+    ''' generate a filename for the image output.
     
-    position: required
+    creates directory if it does not exist.
+    
+    position is required.
     if name and score are specified, filename is:
     <name>-<score>-<pos>.pdf
     '''
@@ -120,7 +211,7 @@ def getfilename(session_id, position, name, score, ext='pdf'):
     if name != '.':
         fname = '%s-%s' % (name, fname)
 
-    imgfilename = path('%s.%s' % (fname, ext))
+    imgfilename = path('%s.%s' % (fname, ext.lower()))
     imgfilepath = imgdir.joinpath(imgfilename)
 
     return imgfilepath
@@ -136,6 +227,19 @@ def parse_options(args):
     parser = OptionParser(usage=usage, version=version,
                           description=description)
 
+    parser.add_option("--img-types", action="append",
+        default=[], help="image types to write. "
+                            "(default: PNG, PDF)")
+
+    parser.add_option("--no-delay", action="store_true",
+        default=False, help="no delay between requests. for debugging "
+                            "only. "
+                            "(default: %default)")
+
+    parser.add_option("--reverse-display", action="store_true",
+        default=False, help="reverse display for neg strand genes "
+                            "(default: %default)")
+
     parser.add_option("-v", "--verbose", action="store_true",
         default=False, help="verbose output (default: %default)")
 
@@ -143,14 +247,20 @@ def parse_options(args):
 
     if len(args) < 2:
         parser.error("specify BED filename and UCSC session ID")
-  
+ 
+    if len(options.img_types) == 0:
+        options.img_types.extend(['PDF','PNG'])
+
     return options, args
 
 def main(args=sys.argv[1:]):
     options, args = parse_options(args)
     bedfilename = args[0]
     sessionid = args[1]
-    kwargs = {'verbose':options.verbose}
+    kwargs = {'verbose':options.verbose,
+              'img_types':options.img_types,
+              'rev_display':options.reverse_display,
+              'no_delay':options.no_delay}
     return ucsc_snapshots(bedfilename, sessionid, **kwargs)
 
 if __name__ == '__main__':
