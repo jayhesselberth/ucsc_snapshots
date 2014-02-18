464fcc750b08ad3981ff17c36a0bc62d69f21daa
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-02-18 04:37:33 -0700

    Update ucsc_snapsnots.py

--- ucsc_snapsnots.py
+++ ucsc_snapsnots.py
@@ -1,31 +1,31 @@
 #! /usr/bin/env python
 
-''' ucsc_snapshots: retrieve pictures from UCSC Genome Browser based on
+''' ucsc_snapshots: retrieve pictures from the UCSC Genome Browser based on
 coordinates specified from BED3+ file and a session ID (hgsid).
 
 UCSC Genome Browser should be set up with tracks prior to using the
 utility and saved as a session. This includes all track settings, sizes etc.
-Once these are setup, you can load the page source and identify the hgsid by
+Once these are setup, load the page source and identify the hgsid by
 searching for "hgsid="
 
 Supply the hgsid and a BED file. Images will be retrieved based on the
-coordinates in the BED file and saved to the directory as:
+coordinates in the BED file and saved to the directory as::
 
-ucsc-snapshots-<hgsid>/chrom-start-end.pdf and
-ucsc-snapshots-<hgsid>/chrom-start-end.png
+    ucsc-snapshots-<hgsid>/chrom-start-end.pdf
+    ucsc-snapshots-<hgsid>/chrom-start-end.png
 
-..note: beware of multiple procs accessessing the same hgsid at
-the same time, they will affect each other's strand settings
+..note::
+
+    beware of multiple procs accessessing the same hgsid at
+    the same time, they will affect each other's strand settings
 
 ..warning::
-if you have many regions, you will need to run this overnight
-(e.g. via a job submitted to the LSF night queue), otherwise UCSC will
-throttle your connection (limit 1 request / 15 sec, 5000 requests per day)
+
+    if you have many regions, you will need to run this overnight
+    (e.g. via a job submitted to the LSF night queue), otherwise UCSC will
+    throttle your connection (limit 1 request / 15 sec, 5000 requests per day)
 
 '''
-
-# TODO:
-# ...
 
 import sys
 import re
